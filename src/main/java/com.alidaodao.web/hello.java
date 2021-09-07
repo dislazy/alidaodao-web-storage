2598a35d62082e8226677d98b509fa126932bd61
jack <ssliunian@yeah.net>
2021-09-07 15:55:25 +0800

    feat(ec): first commit

---
+++ src/main/java/com.alidaodao.web/hello.java
@@ -0,0 +1,16 @@
+package com.alidaodao.web;
+
+/**
+ * @author bosong
+ * @since 2021-09-07
+ */
+public class hello {
+    /**
+     * hello-world
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
