84b966ef19ba421766d89157f333dc79590beba9
bosong <songbo@alidaodao.com>
2022-09-13 14:47:33 +0800

    add stream utils

---
+++ src/main/java/com.alidaodao.web/StreamUtils.java
@@ -0,0 +1,44 @@
+package com.alidaodao.web;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StreamUtils {
+
+    /**
+     * 根据属性去重
+     *
+     * @param keyExtractor
+     * @param <T>
+     * @return
+     */
+    public static <T> Predicate<T> distinct(Function<? super T, ?> keyExtractor) {
+        HashMap<Object, Boolean> map = new HashMap<>();
+        return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
+    /**
+     * 根据多个属性去重
+     *
+     * @param keyExtractors
+     * @param <T>
+     * @return
+     */
+    public static <T> Predicate<T> distinctByKeys(Function<? super T, ?>... keyExtractors) {
+        final Map<List<?>, Boolean> seen = new ConcurrentHashMap<>();
+
+        return t ->
+        {
+            final List<?> keys = Arrays.stream(keyExtractors)
+                    .map(ke -> ke.apply(t))
+                    .collect(Collectors.toList());
+
+            return seen.putIfAbsent(keys, Boolean.TRUE) == null;
+        };
+    }
+
+
+}
