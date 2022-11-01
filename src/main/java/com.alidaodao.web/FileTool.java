e7dcec4218ff68b18ded5b4bd4a15353cb6c7def
bosong <songbo@alidaodao.com>
2022-11-01 15:07:24 +0800

    remove other func

--- src/main/java/com.alidaodao.web/FileTool.java
+++ src/main/java/com.alidaodao.web/FileTool.java
@@ -186,22 +186,5 @@ public class FileTool {
         }
     }
 
-    public static InputStream getResourcesFileInputStream(String fileRelativePath){
-        //获取容器资源解析器
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        try {
-            //获取所有匹配的文件
-            Resource[] resources = resolver.getResources(fileRelativePath);
-            Resource resource = null;
-            if(resources != null && resources.length > 0){
-                resource = resources[0];
-            }
-            //获得文件流，因为在jar文件中，不能直接通过文件资源路径拿到文件，但是可以在jar包中拿到文件流
-            InputStream stream = resource.getInputStream();
-            return stream;
-        } catch (IOException e) {
-            log.warn("读取文件流失败！" + e);
-        }
-        return null;
-    }
+
 }
