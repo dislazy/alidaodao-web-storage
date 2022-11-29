0081c307c4d84b0a4e1ffe27ecfb1ef732995aa1
bosong <songbo@alidaodao.com>
2022-11-29 15:20:34 +0800

    add stream test

---
+++ src/test/java/com.alidaodao.web/StreamUtilsTest.java
@@ -0,0 +1,23 @@
+package com.alidaodao.web;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author songbo
+ * @date 2022-11-29 15:20
+ * @since
+ */
+public class StreamUtilsTest extends TestCase {
+
+    public void testDistinct() {
+        System.out.println("test");
+    }
+
+    public void testDistinctByKeys() {
+        System.out.println("test distiont");
+    }
+}
