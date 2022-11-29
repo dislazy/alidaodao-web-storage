d114fe19601110935202b7a1fbd5403410839cb2
bosong <songbo@alidaodao.com>
2022-11-29 15:18:25 +0800

    add more test

---
+++ src/test/java/com.alidaodao.web/DoubleToolTest.java
@@ -0,0 +1,30 @@
+package com.alidaodao.web;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author songbo
+ * @date 2022-11-29 15:18
+ * @since
+ */
+public class DoubleToolTest extends TestCase {
+
+    public void testAdd() {
+    }
+
+    public void testSub() {
+    }
+
+    public void testMul() {
+    }
+
+    public void testDiv() {
+    }
+
+    public void testTestDiv() {
+    }
+}
---
+++ src/test/java/com.alidaodao.web/PasswordUtilTest.java
@@ -0,0 +1,27 @@
+package com.alidaodao.web;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author songbo
+ * @date 2022-11-29 15:18
+ * @since
+ */
+public class PasswordUtilTest extends TestCase {
+
+    public void testCheckPassword() {
+    }
+
+    public void testGetSalt() {
+    }
+
+    public void testGetStaticSalt() {
+    }
+
+    public void testEncrypt() {
+    }
+}
