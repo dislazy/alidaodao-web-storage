93ceac80919b99429b41d8ff40ecf20a73a25c5a
bosong <songbo@alidaodao.com>
2022-12-02 15:29:10 +0800

    remove number test

---
+++ src/test/java/com.alidaodao.web/NumberToolTest.java
@@ -0,0 +1,31 @@
+package com.alidaodao.web;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author songbo
+ * @date 2022-12-02 15:26
+ * @since
+ */
+public class NumberToolTest extends TestCase {
+
+    public void testRandomEmailCode() {
+        NumberTool.randomEmailCode(10);
+    }
+
+    public void testRandDom() {
+        NumberTool.randDom(1,10);
+    }
+
+    public void testStrToBigDecimal() {
+        try {
+            NumberTool.strToBigDecimal("");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
