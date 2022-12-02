1dbcccbd67261331f3657f8148db396973297440
bosong <songbo@alidaodao.com>
2022-12-02 15:23:52 +0800

    remove main more func

--- src/main/java/com.alidaodao.web/hello.java
+++ src/main/java/com.alidaodao.web/hello.java
@@ -1,8 +1,5 @@
 package com.alidaodao.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author bosong
  * @since 2021-09-07
@@ -15,10 +12,5 @@ public class hello {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
-        List<Integer> ints = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            ints.add(i);
-        }
-        StreamUtils.distinctByKeys(e->e.equals(1));
     }
 }
